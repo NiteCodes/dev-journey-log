e3f99e911553fdf3bba6d3555f05bc27edc44280
NiteCodes <me.evilnitesh2053@gmail.com>
2026-08-06 02:24:41 +0530

    ini

---
+++ JS/script.js
@@ -0,0 +1,659 @@
+// // console.log('it is working');
+// // console.log(1);
+// // console.log(2);
+// // console.log(3);
+// // console.log(4);
+// // console.log(5);
+// // console.log(6);
+
+// // a = 454
+// // console.log(a);
+// // a = 'hello'
+// // console.log(a);
+// // a = true
+// // console.log(a);
+
+// // var arr = [1,2,3,4]
+// // console.log("arr is:",typeof arr);
+
+// // var Obj = {
+// //     name: 'Nitesh',
+// //     age: 25,
+// //     isStudent: true
+// // }
+// // console.log("obj is:",typeof Obj);
+
+
+// // const groups = {
+// //     RED: [],
+// //     GREEN: [],
+// //     ORANGE: [],
+// //     YELLOW: [],
+// //     BLUE: []
+// // };
+
+// // for (let num = 0; num <= 100; num++) {
+
+// //     if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+// //     // console.log(num,": is Blue") 
+// //     groups.BLUE.push(num);
+
+// //      }  else {
+// //         if (num % 2 === 0 && num % 7 === 0) {
+// //         // console.log(num, ":is Yellow")
+// //         groups.YELLOW.push(num);
+
+// //         } else {
+// //            if (num % 2 === 0 && num % 5 === 0) {
+// //                 // console.log(num, ":is ORANGE")
+// //                 groups.ORANGE.push(num);
+        
+// //             } else {
+// //                 if (num % 2 === 0 && num % 3 === 0) {
+// //                     // console.log(num, ":is Green")
+// //                     groups.GREEN.push(num);
+        
+// //                 } else {
+// //                     if (num % 2 === 0) {
+// //                         // console.log(num, ": is Red")
+// //                         groups.RED.push(num);
+
+// //                     }
+
+// //                 }
+// //             }
+// //         }
+// //     }
+// // }
+// // console.log("RED:", groups.RED);
+// // console.log("GREEN:", groups.GREEN);
+// // console.log("ORANGE:", groups.ORANGE);
+// // console.log("YELLOW:", groups.YELLOW);
+// // console.log("BLUE:", groups.BLUE);
+
+
+// // const groups = {
+// //     RED: [],
+// //     GREEN: [],
+// //     ORANGE: [],
+// //     YELLOW: [],
+// //     BLUE: []
+// // };
+
+// // for (let num = 0; num <= 100; num++) {
+// //     if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+// //         groups.BLUE.push(num);
+// //     } else if (num % 2 === 0 && num % 7 === 0) {
+// //         groups.YELLOW.push(num);
+// //     } else if (num % 2 === 0 && num % 5 === 0) {
+// //         groups.ORANGE.push(num);
+// //     } else if (num % 2 === 0 && num % 3 === 0) {
+// //         groups.GREEN.push(num);
+// //     } else if (num % 2 === 0) {
+// //         groups.RED.push(num);
+// //     }
+// // }
+// // // Display grouped results
+// // console.log("RED:", groups.RED);
+// // console.log("GREEN:", groups.GREEN);
+// // console.log("ORANGE:", groups.ORANGE);
+// // console.log("YELLOW:", groups.YELLOW);
+// // console.log("BLUE:", groups.BLUE);
+
+// // for (let num = 0; num <= 100; num++) {
+// //     if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+// //         console.log(num, ": is BLUE");
+// //     } else if (num % 2 === 0 && num % 7 === 0) {
+// //         console.log(num, ": is YELLOW");
+// //     } else if (num % 2 === 0 && num % 5 === 0) {
+// //         console.log(num, ": is ORANGE");
+// //     } else if (num % 2 === 0 && num % 3 === 0) {
+// //         console.log(num, ": is GREEN");
+// //     } else if (num % 2 === 0) {
+// //         console.log(num, ": is RED");
+// //     }
+// // }
+
+// // for (let num = 0; num <= 100; num++) {
+// //     let color = (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) ? "BLUE" :
+// //                 (num % 2 === 0 && num % 7 === 0) ? "YELLOW" :
+// //                 (num % 2 === 0 && num % 5 === 0) ? "ORANGE" :
+// //                 (num % 2 === 0 && num % 3 === 0) ? "GREEN" :
+// //                 (num % 2 === 0) ? "RED" : null;
+
+// //     if (color) console.log(num, ": is", color);
+// // }
+
+// // const num = 30; // pick any number you want to test
+
+// // if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+// //     console.log(num, ": is BLUE");
+// // } else if (num % 2 === 0 && num % 7 === 0) {
+// //     console.log(num, ": is YELLOW");
+// // } else if (num % 2 === 0 && num % 5 === 0) {
+// //     console.log(num, ": is ORANGE");
+// // } else if (num % 2 === 0 && num % 3 === 0) {
+// //     console.log(num, ": is GREEN");
+// // } else if (num % 2 === 0) {
+// //     console.log(num, ": is RED");
+// // }
+
+
+// // const num = 30;
+
+// // (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) ? console.log(num, ": is BLUE") :
+// // (num % 2 === 0 && num % 7 === 0) ? console.log(num, ": is YELLOW") :
+// // (num % 2 === 0 && num % 5 === 0) ? console.log(num, ": is ORANGE") :
+// // (num % 2 === 0 && num % 3 === 0) ? console.log(num, ": is GREEN") :
+// // (num % 2 === 0) ? console.log(num, ": is RED") :
+// // null;
+
+
+// // for (let num = 0; num <= 100; num++) {
+// //     (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) ?
+// //         console.log(num, ": is BLUE") :
+// //     (num % 2 === 0 && num % 7 === 0) ?
+// //         console.log(num, ": is YELLOW") :
+// //      (num % 2 === 0 && num % 5 === 0) ?
+// //         console.log(num, ": is ORANGE"):
+// //      (num % 2 === 0 && num % 3 === 0) ?
+// //         console.log(num, ": is GREEN"):
+// //      (num % 2 === 0) ?
+// //         console.log(num, ": is RED") : 
+// //         null;
+
+// //     }
+
+
+// // even and off
+
+
+// // const groups = {
+// //     even: [],
+// //     odd: [],
+// // };
+
+// // for (let i = 0; i <= 15; i++) {
+// //     (i % 2 === 0) ? groups.even.push(i) : groups.odd.push(i)
+// // }
+
+// // console.log("tirnary operator-")
+// // console.log("Even: ", groups.even);
+// // console.log("Odd: ", groups.odd);
+
+
+// // for (let i = 0; i < 15; i++) {
+// //     if (i % 2 === 0) {
+// //         groups.even.push(i)
+// //     } else {
+// //         groups.odd.push(i)
+// //     }
+// // }
+
+// // console.log("if-else-")
+// // console.log("Even: ", groups.even);
+// // console.log("Odd: ", groups.odd);
+
+
+// // primary and composite number:
+
+
+// // for (let num = 2; num <= 50; num++) {
+// //   let divisorCount = 0;
+
+// //   // check divisibility
+// //   for (let i = 1; i <= num; i++) {
+// //     if (num % i === 0) {
+// //       divisorCount++;
+// //     }
+// //   }
+
+// //   if (divisorCount === 2) {
+// //     console.log(num);
+// //   }
+// // }
+
+
+// // for (let num = 2; num <= 50; num++) {
+// //   let isPrime = true;
+
+// //   for (let i = 2; i < num; i++) {
+// //     if (num % i === 0) {
+// //       isPrime = false;
+// //       break;
+// //     }
+// //   }
+
+// //   if (isPrime) {
+// //     console.log(num);
+// //   }
+// // }
+
+// // wrong
+
+// // for (let num = 2; num <= 50; num++) {
+// //     let factors = 0;
+
+// //     for (let i = 1; i <= 50; i++ ) {
+// //         if (num % i === 0) {
+// //             factors++;
+// //         }
+// //     }  
+
+// //     if (factors === 2) {
+// //         console.log(num, "is Prime")
+
+// //     }
+// // }
+
+
+// // correct
+
+// // for (let num = 2; num <= 50; num++) {
+// //   let factors = 0;
+
+// //   // check divisibility only up to sqrt(num)
+// //   for (let i = 1; i <= Math.sqrt(num); i++) {
+// //     if (num % i === 0) {
+// //       // count both divisors (i and num/i)
+// //       if (i === num / i) {
+// //         factors++; // perfect square divisor counted once
+// //       } else {
+// //         factors += 2; // count both divisors
+// //       }
+// //     }
+// //   }
+
+// //   if (factors === 2) {
+// //     console.log(num, "is Prime");
+// //   } else {
+// //     console.log(num, "is Composite");
+// //   }
+// // }
+
+
+// // wrong
+
+
+// // const num = 100;
+// // const sqrt = Math.ceil(Math.sqrt(num));
+
+// // for (let i = 2; i <= sqrt; i++) {
+// //     if (num % i === 0) {
+// //         console.log(i,"composite");
+// //     } else console.log(i,"prime")
+// // }
+
+
+
+// // write
+
+
+// // for (let num = 2; num <= 10; num++) {
+// //   let isComposite = false;
+
+// //   // check divisibility only up to sqrt(num)
+// //   for (let i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
+// //     if (num % i === 0) {
+// //       isComposite = true;
+// //       break; // found a divisor, no need to continue
+// //     }
+// //   }
+
+// //   if (isComposite) {
+// //     console.log(num, "is Composite");
+// //   } else {
+// //     console.log(num, "is Prime");
+// //   }
+// // }
+
+// // // special case for 1
+// // console.log("1 is neither Prime nor Composite");
+
+
+
+// // array methods
+
+// // const arr = [1,2,3,4,5,6]
+
+// // console.log(typeof arr)
+// // console.log(length.arr) 
+// // console.log(arr);
+// // arr.push(12)
+// // console.log(arr);
+// // arr.pop()
+// // console.log(arr);
+// // arr. unshift(87)
+// // console.log(arr);
+// // arr.pop()
+// // console.log(arr);
+// // // arr.slice(1,4)  //* wrong
+// // // console.log("sliced",arr);
+// // // arr.splice(1,6,99)  //*removed element from 1 to 6 and adds 99
+// // // // console.log("spliced",arr);
+// // const newArr = arr.slice(2,5)
+// // console.log("sliced",newArr);
+// // arr.splice(2,2)
+// // // console.log(arr)
+// // arr.splice(2,2,24)
+// // console.log(arr)
+// // arr.shift(1,2,3,4) //wrong
+// // arr.shift()
+// // console.log("shift",arr)
+// // arr.unshift(24,21,21,21)
+// // console.log("unshift",arr)
+// // console.log(arr.indexOf(4));
+
+
+// // const str = 'hello world'
+// // console.log(str)
+// // const newStr = str.split('o')
+// // console.log(newStr)
+
+
+// // const str = 'hello world'
+// // // const newStr = str.split('l')
+// // // console.log(newStr);
+// // // const joinStr = newStr.join(' ')
+// // // console.log(joinStr);
+
+// // // or 
+
+// // const newStr = str.split('l').sort().reverse().join('')
+// // console.log(newStr);
+
+// // //* its called chaining
+
+
+// // console.log(str[6])
+// // console.log(str.charAt(6))
+
+// // //* both prints character at 6th place in an array
+
+
+// // reverse a string
+
+// // const str = "Hello World";
+// // const newStr = str.split('').reverse().join('');
+// // console.log(newStr);
+
+// // check if a string is pallindrome
+
+// // using built in function
+
+
+// // const str = "dictionary";
+// // const newStr = str.split('').reverse().join('');
+ 
+// // if (str === newStr) {
+// //     console.log(str, "is pallindrome");
+// // } else 
+// //     console.log("not a palindrome");
+
+// // with a program
+
+// // const str = "A man, a plan, a canal: Panama";
+// // console.log("Length =", str.length)
+
+// // const newStr = str.toLowerCase()
+// //         .split(' ').join('')
+// //         .split(',').join('')
+// //         .split(':').join('');
+
+// // console.log(newStr);
+
+// // let isPalindrome = true;
+
+// // for (let i = 0; i < newStr.length/2; i++) {
+
+// //     if ((newStr[i] !== newStr[(newStr.length - 1) - i])) {
+// //         isPalindrome = false;
+// //         break;
+// //     }
+// // }
+
+// // if (isPalindrome) {
+// //     console.log(str, "is palindrome");
+// // } else {
+// //         console.log(str, "is not a palindrome")
+// // }
+
+
+// // anagram 
+
+
+// // const arr = [listen, rescue, stared, finder, 
+// //     danger, rained, stream, earned, plates, cranes];
+
+// // const word1 = "listen";
+// // const word2 = "silent";
+
+// // console.log(word1.split('').sort().join(''))
+// // console.log(word2.split('').sort().join(''))
+
+
+// // if (word1.split('').sort().join('') === word2.split('').sort().join('')) {
+// //     console.log(word1, "->", word2, "is anagram")
+// // } else console.log("not anagram");
+
+
+// // funchtions
+// // 1. named function : (function statement)
+
+// function abc() {
+//     console.log("hello");
+// }
+
+// abc();
+
+
+// // 2. function Expression
+
+// var xyz = function def() {
+//     console.log("'hello hello")  // undefined
+// }
+
+// console.log("value of xyz:", xyz) //8 as a variable 
+// console.log(xyz()) //*as a function '()' is used // undefined
+// // console.log(def); // def is not defined *error*
+
+// // 3. anonymous fuction:
+
+// var ghi = function() {
+//     console.log("this is anonymous function");
+// }
+// console.log(ghi());  // undefined
+
+// // 4. arrow function (ES6 feature = Ecma Scripts 6
+
+// var lmn = () => {
+//     console.log("this is arrow funtion")
+// }
+// console.log(lmn());   // undefined
+// lmn();
+
+
+// var pal = () => console.log("this is also arrow function");
+// pal();
+
+// // eg:
+
+// var sum = (a,b) => a+b;
+// console.log("a + b = ", sum(1,2));
+
+
+
+// // template literals :
+
+// const firstName = 'Nitesh'
+// const lastName = "Prajapati"
+
+// console.log("My first name is", firstName , "and my last name is", lastName);
+// console.log(`My first name is ${firstName} and my last name is ${lastName}`);
+
+// // back tick (``) is   used to access ${} syntax
+
+
+
+
+// console.log(10<20<10);
+
+// console.log(zip(1,2));
+
+// function zip (a,b) {
+//     return a+b;
+// }
+ 
+// // this works because declarations are hoisted 
+// // variables declared with var are also hoisted but only their names and now value
+
+
+// // below code is not allowed because zip is undefined at the time of the call 
+// // because only the variable name is hoisted, not the function itself
+
+// // console.log(zip(1,2)); // ❌ Error: zip is not a function
+
+// // const zip = function(a,b) {
+// //     return a+b;
+// // };
+
+// // closours :
+
+
+// // function outerFunc () {
+// //     const a = 10;
+// //     function innerFunc () {
+// //         var a = 100;
+// //         console.log(a);
+// //     }
+// //     innerFunc();
+// // }
+// // outerFunc();
+
+// // prints 100
+
+
+// // function outerFunc () {
+// //     const a = 10;
+// //     function innerFunc () {
+// //         // var a = 100;
+// //         console.log(a);
+// //     }
+// //     innerFunc();
+// // }
+// // outerFunc();
+
+
+// // prints 100
+
+
+
+
+
+// // function outerFunc () {
+// //     // const a = 10;
+// //     function innerFunc () {
+// //         // var a = 100;
+// //         console.log(a);
+// //     }
+// //     innerFunc();
+// // }
+// // outerFunc();
+
+
+
+// // prints not defined
+
+
+
+// // function innerFunc () {
+// //     var a; // hoisted declaration, initialized as undefined
+// //     console.log(a); // prints undefined since value of a is available but 
+// //                     // not in the scope
+// //     a = 100;
+// // }
+
+console.log("11/07/2016~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+// // func();
+// // function func () {
+// //     console.log("named function")
+
+// // }
+
+
+
+// // func();
+// // const func = function func () {
+// //     console.log('expression function')
+// // }
+
+
+// // func1();
+// // const func1 = function() {
+// //     console.log("anonymous function")
+// // }
+ 
+
+// // func2()
+// // const func2 = () => {
+// //     try {
+// //         console.log("arrow function")
+// //     } catch (error) {
+// //         console.log("cant call function before fuction is defined")        
+// //     }
+
+    
+// // }
+
+
+
+
+
+// // // Higer-order function: first def: a function 
+// // // takes another funtion as an argument
+
+// // const sayHi = (name, callback)=>{
+// //     name(callback);
+// // }
+
+// // // Higer-order function: second def: a function returns another function
+
+
+// const outerFunc = (a) => {
+//     //  return function (b){
+//     function innerFunc (b){
+//         return a+b
+//     }
+//     return innerFunc
+// }
+// console.log(outerFunc(1)(2))
+
+
+// or
+
+
+const outerFunc = (a) => {
+     return function (b){
+        return a+b
+    }
+}
+console.log(outerFunc(1)(2))
+
+
+
+
+
+
+
+// // // call back fucntion
+// // const greet = (callback) => console.log(`Hi ${callback}`);
+// // callback = 'Nitesh';
+// // sayHi (greet,callback);
+
+
+// // // here name is a funtion that is stored as an argumet inside sayHi fuction
+// // // sayHi is a variable that stores a fuction 
+
+
+
